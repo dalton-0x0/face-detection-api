ae5b31f0f681d5969813294a4f8b4b593bc1cded
Dalton Okechukwu <chike911@yahoo.com>
2020-01-12 23:41:16 -0700

    implemented register, signin and id knex server update

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const db = knex({
     database: "smart-brain"
   }
 });
-
+/*
 db.select("*")
   .from("users")
   .then(data => {
     console.log(data);
   });
-
+*/
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -95,49 +95,36 @@ app.post("/register", (req, res) => {
     console.log(hash);
   });
 */
-
-  // below code replaced by knex
-
-  /*
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users[database.users.length - 1]);
-  */
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(404).json("no such user");
-  }
+  //   let found = false;
+  db.select("*")
+    .from("users")
+    .where({
+      id: id
+    })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("user not found");
+      }
+    })
+    .catch(err => res.status(400).json("error loading user. not found."));
 });
 
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json("no such user");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
 });
 
 app.listen(3030, () => {
