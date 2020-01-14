5fdf7e52b6a3b59a46623e3570bff94043603266
Dalton Okechukwu <chike911@yahoo.com>
2020-01-13 22:39:18 -0700

    adding heroku database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-angular-31506",
-    user: "postgres",
-    password: "eden45",
-    database: "smart-brain"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
