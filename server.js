6716fcc8caf3b6dce13d3334f35a66fa1657af42
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-07-01 18:01:30 -0600

    adds gitignore and package-lock

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
-  }
+    ssl: true,
+  },
 });
 
 const app = express();
@@ -46,109 +46,3 @@ app.post("/imageurl", (req, res) => {
 app.listen(process.env.PORT || 3030, () => {
   console.log(`app is running from port ${process.env.PORT}`);
 });
-/*
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "John Doe",
-      email: "john@gmail.com",
-      password: "cookies",
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: "124",
-      name: "Sally Slim",
-      email: "sally@gmail.com",
-      password: "bananas",
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-};
-
-app.get("/", (req, res) => {
-  res.send(database.users);
-});
-
-app.post("/signin", (req, res) => {
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then(user => {
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json("unable to get user"));
-      } else {
-        res.status(400).json("wrong email or password");
-      }
-    })
-    .catch(err => res.status(400).json("wrong email or password"));
-});
-
-app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json("user already exists"));
-});
-
-app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  //   let found = false;
-  db.select("*")
-    .from("users")
-    .where({
-      id: id
-    })
-    .then(user => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("user not found");
-      }
-    })
-    .catch(err => res.status(400).json("error loading user. not found."));
-});
-
-app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json("unable to get entries"));
-});
-*/
